f7fc75ab584afdaf396be5b6b4449c92284156d9
nscheu <scheuring.n@husky.neu.edu>
2017-08-02 01:44:04 -0400

    Just making sure - compiles cleanly

--- app-planner/src/app/mock-plans.ts
+++ app-planner/src/app/mock-plans.ts
@@ -2,41 +2,41 @@ import { Plan } from './plan-model';
 
 export const PLANS: Plan[] = [
   {
-  user_id: 1999991,
-    name: "Twisty",
-  username: 'handleBars',
-  apps: [
-    {   title: 'SampleTitle',
-      components: [
-        {
-          title: 'Node',
-          description: 'NodeJS for Server Tech',
-          url: 'www.nodejs.org',
-          componentTutorials: [
-            {
-              title: 'How to node',
-              url: 'fakesite.fak/howtonode'
-            },
-            {
-              title: 'How to node Even Better!',
-              url: 'fakesite.fak/howtonodeEB'
-            }]
-        },
-        {
-          title: 'Express',
-          description: 'Express JS -- Comms',
-          url: 'www.expressjs.org',
-          componentTutorials: [
-            {
-              title: 'How to Express',
-              url: 'fakesite.fak/howtoexpress'
-            },
-            {
-              title: 'How to Express Even Better!',
-              url: 'fakesite.fak/howtoexpressEB'
-            }]
-        }]
-    } ]
+    user_id: 1999991,
+    name: 'Twisty',
+    username: 'handleBars',
+    applications: [
+      {   title: 'SampleTitle',
+        components: [
+          {
+            title: 'Node',
+            description: 'NodeJS for Server Tech',
+            url: 'www.nodejs.org',
+            componentTutorials: [
+              {
+                title: 'How to node',
+                url: 'fakesite.fak/howtonode'
+              },
+              {
+                title: 'How to node Even Better!',
+                url: 'fakesite.fak/howtonodeEB'
+              }]
+          },
+          {
+            title: 'Express',
+            description: 'Express JS -- Comms',
+            url: 'www.expressjs.org',
+            componentTutorials: [
+              {
+                title: 'How to Express',
+                url: 'fakesite.fak/howtoexpress'
+              },
+              {
+                title: 'How to Express Even Better!',
+                url: 'fakesite.fak/howtoexpressEB'
+              }]
+        } ]
+      }]
   }
 ];
 
